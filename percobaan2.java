2657ce47bdc1c557c403afefa9384b868ec15ffb
daebon07 <muhammadshabran25@gmail.com>
2024-11-28 21:27:33 +0700

    modifikasi untuk pertanyaan percobaan 2 nomor 2

--- percobaan2.java
+++ percobaan2.java
@@ -1,23 +1,30 @@
 import java.util.Scanner;
 
+import java.util.Scanner;
+
 public class percobaan2 {
-    static int hitungpangkat (int x,int y){
-        if (y==0){
+    static int hitungPangkat(int x, int y) {
+        if (y == 0) {
+            System.out.print("1"); 
             return 1;
-        }
-        else {
-            return (x * hitungpangkat (x, y-1));
+        } else {
+            System.out.print(x + (y > 1 ? "x" : ""));
+            return x * hitungPangkat(x, y - 1);
         }
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int bilangan,pangkat;
-        System.out.println("bilangan yang di hitung : ");
-        bilangan= sc.nextInt();
-        System.out.println("pangkat : ");
-        pangkat= sc.nextInt();
-        System.out.println(hitungpangkat(bilangan, pangkat));
+        int bilangan, pangkat;
 
+        System.out.print("Bilangan yang dihitung: ");
+        bilangan = sc.nextInt();
+
+        System.out.print("Pangkat: ");
+        pangkat = sc.nextInt();
+
+        System.out.print("Perhitungan: ");
+        int hasil = hitungPangkat(bilangan, pangkat);
+        System.out.println(" = " + hasil);
     }
-    
 }
